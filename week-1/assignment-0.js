8b5dc909f9c737798c221967a9364409d21f02b4
Kaif khan Md <mdkaifkhan5309@gmail.com>
2024-10-13 21:39:30 +0530

    Complete Week-1-assignments

---
+++ week-1/assignment-0.js
@@ -0,0 +1,5 @@
+const crypto = require('crypto');
+
+const input = "100xdevs";
+const hash = crypto.createHash('sha256').update(input).digest('hex');
+console.log(hash);
---
+++ week-1/assignment-2.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+function findHashWithPrefix(prefix)
+{
+    let input = 0;
+    while(true)
+    {
+        let inputStr = "100xdevs" + input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix))
+        {
+            return { input : inputStr, hash : hash};
+        }
+        input++;
+    }
+}
+
+const result = findHashWithPrefix('00000');
+console.log(`${result.input}`);
+console.log(`${result.hash}`);
